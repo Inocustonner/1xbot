c7ec9b490399382dd0d160f3f6f2afcae98cd37e
Leonov Egor <crozetle@gmail.com>
2023-09-06 17:01:34 +0300

    Работает
    1. Переделал reponse под json

--- 1xbetVilka/Form1.cs
+++ 1xbetVilka/Form1.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace _1xbetVilka {
-
+    
     public struct Match {
         public string GameId;
         public string Liga;
@@ -91,24 +91,24 @@ namespace _1xbetVilka {
                         if (matchBool) {
                             string summ = numericUpDown2.Text;
                             // перенести в make stavka
-                            string otchet_str =
+                            string otchet_str = 
                                 $"ТМ {match.Parameter}, К: {match.Coefficient} " +
                                 $"{match.Command1} - {match.Command2} {summ}";
 
                             Console.WriteLine("Попытка сделать ставку");
-
+                            
                             var stavka = MakeStavka(
-                                wc,
-                                match.Coefficient,
-                                otchet_str,
+                                wc, 
+                                match.Coefficient, 
+                                otchet_str, 
                                 "10",
-                                match.GameId,
-                                domain,
-                                textBox12.Text,
-                                usid1,
-                                summ,
+                                match.GameId, 
+                                domain, 
+                                textBox12.Text, 
+                                usid1, 
+                                summ, 
                                 match.Parameter);
-
+                            
                         }
 
                         Console.WriteLine(""); // Отделяет разные матчи
@@ -162,9 +162,9 @@ namespace _1xbetVilka {
                     wb.Headers.Add("Content-Type", "application/json; charset=UTF-8");
                     wb.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
 
-                    JObject jData = new JObject
+                    JObject jData = new JObject 
                     {
-                        { "coupon", new JObject
+                        { "coupon", new JObject 
                             {
                                 { "Live", true },
                                 {"Summ" , summ },
@@ -202,10 +202,9 @@ namespace _1xbetVilka {
                         break;
                     }
 
-                    Console.WriteLine(jData.ToString());
-
-                    var response = wb.UploadString($"https://{domain}/web-api/datalinelive/putbetscommon",
+                    string response = wb.UploadString($"https://{domain}/web-api/datalinelive/putbetscommon",
                         "POST", jData.ToString());
+                    JObject jResponse;
 
                     if (!f) {
                         break;
@@ -219,35 +218,20 @@ namespace _1xbetVilka {
 
                         Console.WriteLine("Ставка не сделана - код ошибки 1");
                         return false;
+
+                    } else {
+                        jResponse = JObject.Parse(response);
                     }
 
-                    if (response.Contains("betGUID")) {
-                        betGUID = Substring("\"betGUID\":\"", response, "\"");
-                    }
+                    if (jResponse["Value"].GetType().ToString() == "Newtonsoft.Json.Linq.JObject") {
+                        betGUID = (string)jResponse["Value"]["betGUID"];
+                        Thread.Sleep((int)jResponse["Value"]["waitTime"] + 100);
 
-                    if (response.Contains("\"waitTime\"")) {
-                        var waitTime = Substring("\"waitTime\":", response, "}");
-                        if (waitTime != "") {
-                            Thread.Sleep(Convert.ToInt32(waitTime) + 100);
-                        }
-                    }
-
-                    if (response.Contains("\"Success\":true") & response.Contains("\"waitTime\":0}"))//
-                    {
-
-                        this.Invoke((MethodInvoker)delegate () {
-                            textBox9.AppendText(" OK" + Environment.NewLine);
-                        });
-
-                        linksold += game_id + " ";
-
-                        return true;
-
-                    } else if (!response.Contains("Error\":\"\"")) {
-                        var err = Substring("Error\":\"", response, "\"");
-                        if (!textBox9.Text.Contains(txtlink + " " + err)) {
+                    } else if ((string)jResponse["Error"] != null) {
+                        string error = (string)jResponse["Error"];
+                        if (!textBox9.Text.Contains(txtlink + " " + error)) {
                             Invoke((MethodInvoker)delegate () {
-                                textBox9.AppendText(" " + err + Environment.NewLine);
+                                textBox9.AppendText(" " + error + Environment.NewLine);
                             });
 
                             Console.WriteLine("Ставка не сделана - код ошибки 2");
@@ -263,6 +247,18 @@ namespace _1xbetVilka {
                         Console.WriteLine("Ставка не сделана - код ошибки 3");
                         return false;
                     }
+
+                    if ((bool)jResponse["Success"] == true & (string)jResponse["Value"]["waitTime"] == "0")
+                    {
+                        this.Invoke((MethodInvoker)delegate () {
+                            textBox9.AppendText(" OK" + Environment.NewLine);
+                        });
+
+                        linksold += game_id + " ";
+
+                        return true;
+
+                    }
                 }
             }
             catch (Exception er) {
@@ -273,6 +269,9 @@ namespace _1xbetVilka {
                 return false;
             }
 
+            Invoke((MethodInvoker)delegate () {
+                textBox9.AppendText("Fail" + Environment.NewLine);
+            });
             Console.WriteLine("Ставка не сделана - код ошибки 5");
             return false;
         }
